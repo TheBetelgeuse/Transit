861309e9ed33d298a49eae2dcc1f1d45637f0284
Gleb Boiko <glebboyko@icloud.com>
2022-12-12 12:36:26 +0300

    Приведенено к соответствию новой логике

--- initialization.hpp
+++ initialization.hpp
@@ -1,8 +1,12 @@
-//
-// Created by Глеб Бойко on 12.12.2022.
-//
-
 #ifndef TRANSIT__INITIALIZATION_HPP_
 #define TRANSIT__INITIALIZATION_HPP_
 
-#endif //TRANSIT__INITIALIZATION_HPP_
+#include "traffic_controller.hpp"
+#include "truck.hpp"
+
+enum Location {
+  mine = 0,
+  fabric = 2
+};
+
+#endif  // TRANSIT__INITIALIZATION_HPP_
