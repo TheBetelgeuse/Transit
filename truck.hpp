56f76dc381752fe6dc908dd0c95a43899955ecd7
Gleb Boiko <glebboyko@icloud.com>
2022-12-13 22:16:06 +0300

    Аян: Объявлен класс Truck и его метод StartProcess

--- truck.hpp
+++ truck.hpp
@@ -1,4 +1,13 @@
 #ifndef TRANSIT__TRUCK_HPP_
 #define TRANSIT__TRUCK_HPP_
 
-#endif  // TRANSIT__TRUCK_HPP_
+namespace TruckNS {
+
+class Truck {
+ public:
+  Truck(bool, int, int, int, int);
+  void StartProcess();
+};
+} // namespace TruckNS
+
+#endif // TRANSIT__TRUCK_HPP_
