bc3498d8c4a25d0b81183e268d63f51545f9dc6d
TheBetelgeuse <the.cyber.neo@gmail.com>
2022-12-14 14:26:43 +0300

    fixed truck files

--- truck.cpp
+++ truck.cpp
@@ -1,7 +1,7 @@
 class Truck {
  public:
 
-  Truck(int speed, int weight, bool position);
+  Truck(bool init_location, int number, int weight, int speed, int lenght);
   int GetSpeed() const { return speed_; }
   int GetWeight() const { return weight_; }
   bool GetPosition() const { return position_; }
@@ -10,11 +10,7 @@ class Truck {
   int speed_;
   int weight_;
   bool position_; // До фабрики – 1, обратно – 0
+  int index_;
 };
 
 
-Truck::Truck(int speed, int weight, bool position) {
-  speed_ = speed;
-  weight_ = weight;
-  position_ = position;
-}
--- truck.hpp
+++ truck.hpp
@@ -1,13 +1,48 @@
 #ifndef TRANSIT__TRUCK_HPP_
 #define TRANSIT__TRUCK_HPP_
 
+#include "sem_and_queue.hpp"
+#include "initialization.hpp"
+#include <optional>
+
 namespace TruckNS {
 
 class Truck {
  public:
-  Truck(bool, int, int, int, int);
+  Truck(bool init_location, int number, int weight, int speed, int lenght);
+  int GetSpeed() const { return speed_; }
+  int GetWeight() const { return weight_; }
+  bool GetPosition() const { return init_location_; }
+
+ private:
+  int speed_;
+  int weight_;
+  bool init_location_;
+  int index_;
+  int lenght_;
   void StartProcess();
 };
+
+Truck::Truck(bool init_location, int number, int weight, int speed, int lenght) {
+  speed_ = speed;
+  weight_ = weight;
+  init_location_ = init_location;
+  index_ = number;
+  lenght_ = lenght;
+
+  const char* pathname = "transit";
+  Semaphore end(2 ,ftok(pathname, 7));
+
+}
+void Truck::StartProcess() {
+  while (1) {
+    if (init_location_ == 0) {
+
+    }
+  }
+
+}
 } // namespace TruckNS
+
 
 #endif // TRANSIT__TRUCK_HPP_
