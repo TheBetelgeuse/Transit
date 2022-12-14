7b13a7d4041d5b914cb1f994b7537a5708967c4a
Gleb Boiko <glebboyko@icloud.com>
2022-12-15 01:22:47 +0300

    изменена логика, добавлено логирование

--- initialization.cpp
+++ initialization.cpp
@@ -1,58 +1,75 @@
+#include <unistd.h>
+
 #include <iostream>
-#include "truck.hpp"
+#include <vector>
+
 #include "traffic_controller.hpp"
+#include "truck.hpp"
 
 int main() {
   SystemFile file;
   file.openf("LogInitializator.txt");
+  std::vector<char> logs(200);
 
-  int max_load;
-  int length;
-  int max_number_of_trucks;
+  int max_load, length, max_number_of_trucks;
   std::cin >> max_load >> length >> max_number_of_trucks;
+  sprintf(logs.data(),
+          "Максимальная нагрузка - %d, длина моста - %d, максимальное "
+          "количество грузовиков - %d\n",
+          max_load, length, max_number_of_trucks);
+  file.writef(logs.data(), std::strlen(logs.data()));
 
-  std::string bridge = "Задан мост с такими параметрами ";
-  bridge += max_load;
-  bridge += length;
-  bridge += max_number_of_trucks;
-  bridge += "\n";
-  file.writef(bridge.c_str(), bridge.length() + 1);
+  std::vector<TCNS::TrafficController*> traffic_controllers(2, nullptr);
+  for (int i = 0; i < 2; ++i) {
+    traffic_controllers[i] =
+        new TCNS::TrafficController{bool(i), max_load, max_number_of_trucks};
+
+    logs = std::vector<char>(200);
+    sprintf(logs.data(), "Создан регулировщик № %d\n", i);
+    file.writef(logs.data(), std::strlen(logs.data()));
+  }
+
+  int init_number_of_trucks;
+  std::cin >> init_number_of_trucks;
+  std::vector<TruckNS::Truck*> trucks(init_number_of_trucks, nullptr);
+  for (int i = 0; i < init_number_of_trucks; ++i) {
+    bool location;
+    int weight, speed;
+    std::cin >> location >> weight >> speed;
+    trucks[i] = new TruckNS::Truck{location, i, weight, speed, length};
+
+    logs = std::vector<char>(200);
+    sprintf(logs.data(),
+            "Создан грузовик № %d: масса - %d, скорость - %d, местоположение "
+            "-  %d\n",
+            i, weight, speed, location);
+    file.writef(logs.data(), std::strlen(logs.data()));
+  }
 
   int pid;
   for (int i = 0; i < 2; ++i) {
     pid = fork();
     if (pid == 0) {
-      TCNS::TrafficController reg(i, max_load, max_number_of_trucks);
-      std::string log = "Создан регулировщик номер \n";
-      log += char(i);
-      file.writef(log.c_str(), log.length() + 1);
-      reg.StartProcess();
-      return 0;
+      traffic_controllers[i]->StartProcess();
+      exit(0);
     }
   }
 
-  int init_number_of_trucks;
-  std::cin >> init_number_of_trucks;
   for (int i = 0; i < init_number_of_trucks; ++i) {
     pid = fork();
     if (pid == 0) {
-      int location;
-      int weight;
-      int speed;
-      std::cin >> location >> weight >> speed;
-      TruckNS::Truck truck(location, i, weight, speed, length);
-      std::string log = "Cоздан грузовик номер ";
-      log += i;
-      log += "со скоростью ";
-      log += speed;
-      log += " и местоположением ";
-      log += location;
-      log += "\n";
-      file.writef(log.c_str(), log.length() + 1);
-      truck.StartProcess();
-      return 0;
+      trucks[i]->StartProcess();
+      exit(0);
     }
   }
+
+  for (int i = 0; i < traffic_controllers.size(); ++i) {
+    delete traffic_controllers[i];
+  }
+  for (int i = 0; i < trucks.size(); ++i) {
+    delete trucks[i];
+  }
+
   /*******************************************************/
   for (int i = 0; i < init_number_of_trucks + 2; ++i) {
     int tmp;
