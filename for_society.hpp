6d3b4465eecc1a289ad19b6289163dcb11162c08
Gleb Boiko <glebboyko@icloud.com>
2022-12-14 17:40:25 +0300

    aka initialization.hpp

---
+++ for_society.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+const char kLogFile[] = "transit";
+
+enum Location { mine = 0, factory = 1 };
+
+std::string IntToString(int number);
--- initialization.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef TRANSIT__INITIALIZATION_HPP_
-#define TRANSIT__INITIALIZATION_HPP_
-
-const char kLogFile[] = "transit";
-
-enum Location { mine = 0, factory = 1 };
-
-#endif  // TRANSIT__INITIALIZATION_HPP_
