7ecb1a9e0f3daf461067bccfda77e61ce5da69c1
TheBetelgeuse <the.cyber.neo@gmail.com>
2022-12-15 17:43:00 +0300

    Fixed output

--- truck.cpp
+++ truck.cpp
@@ -1,15 +1,15 @@
 #include "truck.hpp"
 #include <string>
 
-TruckNS::Truck::Truck(bool init_location, int number, int weight, int speed, int lenght) {
+TruckNS::Truck::Truck(bool init_location, int number, int weight, int speed,
+                      int lenght) {
   speed_ = speed;
   weight_ = weight;
   init_location_ = init_location;
   index_ = number;
   lenght_ = lenght;
 
-
-  quantity = Semaphore(2 ,ftok(kPCFile, 7));
+  quantity = Semaphore(2, ftok(kPCFile, 7));
   quantity.Operation(0, 1, false);
   end = Semaphore(1, ftok(kPCFile, 6));
   main = Semaphore(1, ftok(kPCFile, 3));
@@ -19,10 +19,11 @@ TruckNS::Truck::Truck(bool init_location, int number, int weight, int speed, int
   file.openf("LogTruck.txt");
   std::vector<char> log(200);
   sprintf(log.data(),
-          "Был создан самосвал с такими параметрами:\nСкорость – %d; номер самосвала - %d;"
-          " вес самосвала – %d; направление – %d\n",
+          "Был создан самосвал с такими параметрами:\nСкорость – %d; номер "
+          "самосвала - %d"
+          " вес самосвала – %d направление – %d\n",
           speed, number, weight, init_location);
-  file.writef(log.data(),  std::strlen(log.data()));
+  file.writef(log.data(), std::strlen(log.data()));
 }
 
 void TruckNS::Truck::StartProcess() {
@@ -31,22 +32,30 @@ void TruckNS::Truck::StartProcess() {
       EndProcess();
     } else {
       try {
-        init_location_ ? one_controller.Send({index_, weight_}, 1) : zero_controller.Send({index_, weight_,}, 1);
+        init_location_ ? one_controller.Send({index_, weight_}, 1)
+                       : zero_controller.Send(
+                             {
+                                 index_,
+                                 weight_,
+                             },
+                             1);
         std::vector<char> log(200);
         sprintf(log.data(),
-                "Самосвал номер %d подъехал к регулировщику номер %d\n",
-                index_, init_location_ ? 1 : 0);
+                "Самосвал номер %d подъехал к регулировщику номер %s", index_,
+                init_location_ ? "1\n" : "0\n");
         file.writef(log.data(), std::strlen(log.data()));
       } catch (int) {
         EndProcess();
         exit(404);
       }
       try {
-        init_location_ ? one_controller.Receive(index_ + 2, true) : zero_controller.Receive(index_ + 2, true);
+        init_location_ ? one_controller.Receive(index_ + 2, true)
+                       : zero_controller.Receive(index_ + 2, true);
         std::vector<char> log(200);
         sprintf(log.data(),
-                "Самосвал номер %d получил разрешение на проезд от регулировщика номер %d\n",
-                index_, init_location_ ? 1 : 0);
+                "Самосвал номер %d получил разрешение на проед от "
+                "регулировщика номер %d\n",
+                index_, 123);
         file.writef(log.data(), std::strlen(log.data()));
       } catch (int error) {
         if (error == EINTR) {
@@ -54,18 +63,20 @@ void TruckNS::Truck::StartProcess() {
           return;
         } else {
           EndProcess();
-          exit (404);
+          exit(404);
         }
       }
       try {
-        init_location_ ? factory.Operation(0, -1, false) : main.Operation(0, -1, false);
+        init_location_ ? factory.Operation(0, -1, false)
+                       : main.Operation(0, -1, false);
         std::vector<char> log(200);
         sprintf(log.data(),
-                "Самосвал номер %d проехал мост и передал об этом сообщение регулировщику номер %d\n",
-                index_, init_location_ ? 1 : 0);
+                "Самосвал номер %d проехал мост и передал об этом сообщение "
+                "регулировщику номер %s",
+                index_, init_location_ ? "1\n" : "0\n");
         file.writef(log.data(), std::strlen(log.data()));
       } catch (int) {
-        exit (404);
+        exit(404);
       }
       init_location_ = !init_location_;
     }
